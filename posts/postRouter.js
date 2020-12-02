c31e38d98d8f048f44c7a3ac2688f2bd26eb37c7
Brandon Sievert (slick) <brandonwsievert@gmail.com>
2020-12-02 18:23:49 -0500

    All requests working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,74 @@
 const express = require('express');
+const middleware = require('../middleware/middleware');
+const Post = require('./postDb')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // do your magic!
+  try{
+    const post = await Post.get()
+    if (post) {
+      res.status(200).json(post)
+    } else {
+      res.status(500).json({
+        message: 'Posts get request failed'
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    })
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', middleware.validatePostId, async (req, res) => {
   // do your magic!
+  const { id } = req.params
+  const post = await Post.getById(id)
+  res.status(200).json(post)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', middleware.validatePostId, (req, res) => {
   // do your magic!
+  Post.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: `User with name of ${req.body.name} had their post with id of ${req.params.id} deleted!`
+        })
+      } else {
+        res.status(404).json({
+          message: 'The post could not be found'
+        })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'Error in removing the post'
+      })
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', middleware.validatePost, (req, res) => {
   // do your magic!
+  Post.update(req.params.id, req.body)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({
+          message: `Post with id of ${req.params.id} could not be found`
+        })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'Error updating the post'
+      })
+    })
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const middleware = require('./middleware/middleware')
 const server = express();
+
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(express.json())
+server.use(middleware.logger)
+
+server.use('/api/users', userRouter)
+server.use('/api/posts', postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
-//custom middleware
-
-function logger(req, res, next) {}
 
 module.exports = server;
